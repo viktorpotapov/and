3c967ef3bffc270c68656cb8ad55d1448ab4c876
viktorpotapov <90562212+viktorpotapov@users.noreply.github.com>
2021-09-12 17:22:21 +0300

    sd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
         int s;
+        int a;
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
